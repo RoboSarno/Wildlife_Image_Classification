95531e29c555660253d93065ba4a4ebd5ed16e7d
Franco Sanchez <22228100+Femli@users.noreply.github.com>
2022-08-30 07:30:53 -0700

    updating streamlit app for production (#20)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+from ast import arg
+from turtle import onclick
 from PIL import Image
 from keras.models import model_from_json
 import numpy as np
@@ -9,13 +11,16 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
-subdir = './model_data'
-filename = 'saved_model.pb'
+from config import MODEL_DIR, WEIGHTS_FILE, IMAGE_REPO_URL
+from utils import reformat_img, test_model
+
+subdir = MODEL_DIR
+filename = WEIGHTS_FILE
 checkpoint_dir = os.path.dirname('./model_data_h5/model_es.h5')
 
 
 
-st.set_page_config(page_title="Wild Image Classifier App",
+st.set_page_config(page_title="Wildlife Image Classifier App",
                   page_icon="🧊",layout="wide", initial_sidebar_state="expanded"
  )
 
@@ -36,44 +41,21 @@ def columns():
     with col4:
         image = Image.open('./images/Precision.png')
         st.image(image)
-        
-def reformat_img(image_file):
-    img = Image.open(image_file)
-    # width, height = img.size
-    # print(width, height)
-    # st.image(img)
-    img = img.resize((256, 256))
     
-    #img_arr = img_to_array(img)
-    img_arr = np.reshape(img, (256, 256, 3))
-    img_arr = np.expand_dims(img_arr, axis=0) #required structured for preds
-    # img_arr = img_arr / 256
-    return img_arr
-    
-
-def test_model(loaded_model, img):
-    print('-----------------------------------')
-    class_labels = ['antelope_duiker','bird','blank','civet_genet', 'hog',
-            'leopard','monkey_prosimian','rodent']
-    prediction_probs = loaded_model.predict(img)
-    predicted_label = class_labels[np.argmax(prediction_probs)]
-    return predicted_label, prediction_probs
         
-
-
 @st.cache(allow_output_mutation=True)
 def load_model():
     return tf.keras.models.load_model('./model_data_h5/final_model')
 
-
-    
-
 def description():
     st.write('''
-                Hello, welcome to our application: 
+                Welcome to our classifier app! 
              
-                Below you can see Our Models Preformance for train and validation set for Loss, Accuracy, Recall, and Precision.
-                Go ahead and Test Our Models Preformance on the Sidebar.
+                On the left side you can download an image from our server that contains an image that the model has never seen before.
+                Afer you download that image, you can upload it and have it classified.
+                
+                Our overall model performance on training and validation data is found below. 
+                You will see benchmarks on loss, accuracy, recall and precision.
              ''')
 
 def main():
@@ -81,9 +63,10 @@ def main():
     animal_perc = None
     model = load_model()
     with st.sidebar:
-        st.subheader('Test our Model:')
-        image_file = st.file_uploader("Upload Images", type=["jpg","jpeg"])
-
+        st.subheader('Utilities')
+        st.markdown(f"Download an image [here]({IMAGE_REPO_URL})")
+        image_file = st.file_uploader("Upload image here:", type=["jpg","jpeg"])
+        
         if st.button('Run Model'):
             with st.spinner('Wait for it...'):
                 if image_file is not None:
@@ -93,15 +76,16 @@ def main():
                     
                     time.sleep(5)    
                     
-    st.title('Wild Image Classifier')
+    st.title('Wildlife Image Classifier')
+    if prediction is not None and animal_perc is not None:
+        tags = ['antelope_duiker', 'bird', 'blank', 'civet_genet', 'hog', 'leopard', 'monkey_prosimian', 'rodent']
+        st.subheader("Class probabilities")
+        st.dataframe(pd.DataFrame(animal_perc.round(2), columns=tags))
+        st.markdown(f'Predicted class: **{prediction}**')
     st.write('''---''')
     description()
     st.write('''---''')
     columns()
-    if prediction is not None and animal_perc is not None:
-        
-        st.dataframe(pd.DataFrame(animal_perc.round(2), columns=['antelope_duiker', 'bird', 'blank', 'civet_genet', 'hog', 'leopard', 'monkey_prosimian', 'rodent']))
-        st.write(f'The models prediction is...{prediction}')
     
 if __name__ == '__main__':
     main()
